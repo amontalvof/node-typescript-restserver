fadca3fdfcfaec5b5999030f8cc94c95c04156f1
amontalvof <amontalvof92@gmail.com>
2021-06-04 11:04:47 -0500

    feat: completed CRUD

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,26 +1,76 @@
 import { Request, Response } from 'express';
+import User from '../models/user';
 
-export const getUsers = (req: Request, res: Response) => {
-    res.json({ msg: 'getUsers' });
+export const getUsers = async (req: Request, res: Response) => {
+    const users = await User.findAll();
+    res.json({ users });
 };
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
     const { id } = req.params;
-    res.json({ msg: 'getUser', id });
+    const user = await User.findByPk(id);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({
+            msg: `There is no user with the id “${id}” in the database.`,
+        });
+    }
 };
 
-export const postUser = (req: Request, res: Response) => {
+export const postUser = async (req: Request, res: Response) => {
     const { body } = req;
-    res.json({ msg: 'postUser', body });
+    try {
+        const emailExists = await User.findOne({
+            where: {
+                email: body.email,
+            },
+        });
+        if (emailExists) {
+            return res.status(400).json({
+                msg: `There is already a user with the email ${body.email}`,
+            });
+        }
+        const user = User.build(body);
+        await user.save();
+        res.json({ msg: 'postUser', body });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Talk to the administrator',
+        });
+    }
 };
 
-export const putUser = (req: Request, res: Response) => {
+export const putUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { body } = req;
-    res.json({ msg: 'putUser', body, id });
+    try {
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(400).json({
+                msg: `There is no user with the id “${id}” in the database.`,
+            });
+        }
+        await user.update(body);
+        res.json({ user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Talk to the administrator',
+        });
+    }
 };
 
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
     const { id } = req.params;
-    res.json({ msg: 'deleteUser', id });
+    const user = await User.findByPk(id);
+    if (!user) {
+        return res.status(400).json({
+            msg: `There is no user with the id “${id}” in the database.`,
+        });
+    }
+    // await user.destroy();
+    await user.update({ status: false });
+    res.json({ user });
 };
